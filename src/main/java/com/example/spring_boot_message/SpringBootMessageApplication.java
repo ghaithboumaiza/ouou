3e8bb65bbc3c6e769459f06708962a1a975c6df8
oussmaa <gharianioussama24@gmail.com>
2023-11-23 23:17:13 +0100

    neu3

---
+++ src/main/java/com/example/spring_boot_message/SpringBootMessageApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_message;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMessageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMessageApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/spring_boot_message/testing.java
@@ -0,0 +1,23 @@
+package com.example.spring_boot_message;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+@CrossOrigin
+public class testing {
+
+
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String getAllProduit() {
+        //r//eturn produitService.getAllProduit();
+        return  "I love u Baby <3 <3 ";
+    }
+
+
+
+}
